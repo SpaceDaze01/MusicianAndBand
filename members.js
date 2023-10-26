bf275a70ea3980a2f9365c414b7575d33817e638
SpaceDaze01 <luunaastar@gmail.com>
2023-10-26 15:27:15 +0000

    update
    update

--- members.js
+++ members.js
@@ -90,3 +90,138 @@ class Member {
 }
 
 
+
+class instruments {
+
+  //instruments 
+  addInstrumentsToList(newInstrument) {
+    if (newInstrument.length < 3 || newInstrument.length > 25) {
+      console.log("You must write between 2-20 symbols!");
+    } else if (this.savingBands.includes(newInstrument)) {
+      console.log(`${newInstrument} already exists!`);
+    } else {
+      this.savingBands.push(new Instrument(newInstrument));
+      this.updateFile()
+    }
+  }
+
+
+  setInstruments(instruments) {
+    for (let i = 0; i < instruments.length; i++) {
+      const index = this.savingBands.findIndex(element => element.name === instruments[i]);
+      console.log("Index - ", index);
+      if (index >= 0) {
+        this.pickInstruments(index);
+      }
+    }
+  }
+
+  printInstrumentsList() {
+    for (let i = 0; i < this.list.length; i++) {
+      console.log(`${i + 1}. ${this.list[i].name} - ${this.list[i].picked}`);
+    }
+  }
+
+
+  pickInstruments(index) {
+    this.list[index].picked = !this.list[index].picked;
+  }
+
+
+  listInstrumentsLength() {
+    return this.list.length;
+  }
+
+  listOfPickedInstruments() {
+    const templist = [];
+
+    for (let i = 0; i < this.list.length; i++) {
+      if (this.list[i].picked) {
+        templist.push(this.list[i].name)
+      }
+    }
+    return templist;
+  } //slut instruments
+}
+
+
+
+class Instrument {
+
+  name;
+  picked;
+
+  constructor(name, picked = false) {
+    this.name = name;
+    this.picked = picked;
+  }
+}
+
+
+
+
+
+class EarlierMembers {
+//earliermembers
+addOldMemberToList(oldMember) {
+  if (oldMember.length < 3 || oldMember.length > 25) {
+    console.log("You must write between 2-20 symbols!");
+  } else if (this.savingBands.includes(oldMember)) {
+    console.log(`${oldMember} already exists!`);
+  } else {
+    this.savingBands.push(new EarlierMember(oldMember));
+    this.updateFile()
+  }
+}
+
+
+setOldMember(earlierMembers) {
+  for (let i = 0; i < earlierMembers.length; i++) {
+    const index = this.savingBands.findIndex(element => element.name === earlierMembers[i]);
+    console.log("Index - ", index);
+    if (index >= 0) {
+      this.pickOldMember(index);
+    }
+  }
+}
+
+printOldMemberList() {
+  for (let i = 0; i < this.list.length; i++) {
+    console.log(`${i + 1}. ${this.list[i].name} - ${this.list[i].picked}`);
+  }
+}
+
+
+pickOldMember(index) {
+  this.list[index].picked = !this.list[index].picked;
+}
+
+
+listOldMemberLength() {
+  return this.list.length;
+}
+
+listOfPickedOldMembers() {
+  const templist = [];
+
+  for (let i = 0; i < this.list.length; i++) {
+    if (this.list[i].picked) {
+      templist.push(this.list[i].name)
+    }
+  }
+  return templist;
+} //slut på earliermember
+
+}
+
+
+class EarlierMember {
+
+  name;
+  picked;
+
+  constructor(name, picked = false) {
+    this.name = name;
+    this.picked = picked;
+  }
+}
